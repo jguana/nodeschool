c2ededb3f9f8a64d24d594f28f52053f6d67635a
John Kim <kim.jy.john@gmail.com>
2018-09-25 13:59:41 -0700

    Complete juggling-async

--- learnyounode/juggling-async.js
+++ learnyounode/juggling-async.js
@@ -1,1 +1,32 @@
-var http = require('http')
+const bl = require('bl')
+const http = require('http')
+const results = []
+let count = 0
+
+function printResults() {
+    for (let i = 0; i < 3; i++) {
+        console.log(results[i])
+    }
+}
+
+function httpGet (index) {
+    http.get(process.argv[2+index], function (res) {
+        res.pipe(bl(function (err, data) {
+            if (err) {
+                console.error(err)
+            // ? or console.error
+            }
+
+            results[index] = data.toString()
+            count++
+
+            if (count === 3) {
+                printResults()
+            }
+        }))
+    })
+}
+
+for (i=0; i<3; i++) {
+    httpGet(i)
+}
