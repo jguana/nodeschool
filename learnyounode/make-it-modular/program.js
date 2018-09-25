8a6bf1ce883b4af1563a4cec0ac814323ad8d275
John Kim <kim.jy.john@gmail.com>
2018-09-25 13:28:46 -0700

    Complete learnyounode

--- learnyounode/make-it-modular/program.js
+++ learnyounode/make-it-modular/program.js
@@ -1,8 +1,13 @@
-var mymodule = require('./program')
-
+var filter = require('./module.js')
 var dir = process.argv[2]
-var ext = '.' + process.argv[3]
+var ext = process.argv[3]
 
-mymodule(dir, ext, callback) {
+filter(dir, ext, function (err, list) {
+  if (err) {
+    return console.error('There was an error:', err)
+  }
 
-}
+  list.forEach(function (file) {
+    console.log(file)
+  })
+})
